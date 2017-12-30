d1999deaf28e63ceedd1d35528514bb8945ec2c2
rohit83is <rohit.83.is@gmail.com>
2017-12-31 03:00:02 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,17 +1,16 @@
 run_analysis <- function() {
   library("dplyr")
-
+  
   ## Download and unzip the dataset:
-   if (!file.exists(filename)){
-      fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
-      download.file(fileURL, filename, method="curl")
-     }  
-   if (!file.exists("UCI HAR Dataset")) { 
-     unzip(filename) 
-    }
-
+  download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", dest="dataset.zip", mode="wb")
+  
+  
+  if (!file.exists("UCI HAR Dataset")) { 
+    unzip ("dataset.zip")
+  }
+  
   #Read all the files into memory
-
+  
   x_test <- read.table("~/R/UCI HAR Dataset/test/X_test.txt") 
   y_test <- read.table("~/R/UCI HAR Dataset/test/Y_test.txt")
   subject_test <- read.table("~/R/UCI HAR Dataset/test/subject_test.txt")
@@ -23,7 +22,7 @@ run_analysis <- function() {
   
   
   #Add Activity & Subject information to the Test Dataset.Convert Activity columns from numbers to actual activity name.
-
+  
   colnames(x_test) <- features$V2
   temp_test <- cbind(y_test,x_test)
   colnames(temp_test)[1] <- "activity_name"
@@ -36,10 +35,10 @@ run_analysis <- function() {
   temp_test$`activity_name`[temp_test$`activity_name` == 4] <- "SITTING"
   temp_test$`activity_name`[temp_test$`activity_name` == 5] <- "STANDING"
   temp_test$`activity_name`[temp_test$`activity_name` == 6] <- "LAYING"
-
-    
+  
+  
   #Add Activity & Subject information to the Train Dataset.Convert Activity columns from numbers to actual activity name.
-
+  
   colnames(x_train) <- features$V2
   temp_train <- cbind(y_train,x_train)
   colnames(temp_train)[1] <- "activity_name"
@@ -53,17 +52,18 @@ run_analysis <- function() {
   temp_train$`activity_name`[temp_train$`activity_name` == 5] <- "STANDING"
   temp_train$`activity_name`[temp_train$`activity_name` == 6] <- "LAYING"
   
-
+  
   #Merge Test and Train datasets
-
+  
   final <- rbind(temp_test,temp_train)
   # Select rows which measure Mean and STD
   pattern <-  grep("[Mm]ean|std", names(final),value = TRUE)
   final_2 <- subset(final, select = c("subject_test","activity_name",pattern))
-   
+  
   # Summarize the final dataset
-
+  
   aggr <- group_by(final_2,subject_test,activity_name)
   sum <- summarize_all(aggr,funs(mean))
-  str(sum) 
-
+  write.table(sum,"tidy.txt",row.names = FALSE)
+  
+}
